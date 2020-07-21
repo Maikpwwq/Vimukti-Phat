7f85d3ac4a8b9758f1d98cfaeeb53b95a6599aae
Mike_col <mariasf@correo.udistrital.edu.co>
2020-07-20 21:31:12 -0500

    Niveles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Vimukti-Phat';
+
+  constructor () {
+
+    
+  }
+
+
+
+  /* Or with jQuery
+
+  var instance = M.Carousel.init({
+      fullWidth: true,
+      indicators: true
+    });
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });/*
+
+  /* Parallax
+
+  AppComponent.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, options);
+  }); */
 }
